d4774ef6fa67345f2135e42adaf00db44ac797f8
Christoffer <cnis@bankingcircle.com>
2025-01-03 15:51:14 +0100

    re-arrange packages

--- home-manager/configs/de.nix
+++ home-manager/configs/de.nix
@@ -7,10 +7,13 @@
 
 {
   imports = [
+    ./alacritty.nix
     ./hyprland.nix
     ./i3.nix
+    ./kitty.nix
     ./polybar.nix
     ./rofi.nix
     ./swaylock.nix
+    ./vscode.nix
   ];
 }
--- home-manager/configs/main.nix
+++ home-manager/configs/main.nix
@@ -7,13 +7,9 @@
 
 {
   imports = [
-    ./alacritty.nix
-    ./direnv.nix
     ./git.nix
-    ./kitty.nix
     ./neovim.nix
     ./nixpkgs.nix
-    ./vscode.nix
     ./zsh.nix
   ];
 }
