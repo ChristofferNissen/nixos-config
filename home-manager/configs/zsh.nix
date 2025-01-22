25e1c257b98b1cc8e182671209c576c210eab5fe
Christoffer Nissen <cnis@bankingcircle.com>
2025-01-22 10:31:50 +0100

    zsh

--- home-manager/configs/zsh.nix
+++ home-manager/configs/zsh.nix
@@ -17,7 +17,7 @@
   programs.zsh.initExtra = ''
     source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
     source ~/.p10k.zsh
-    export YSU_MESSAGE_POSITION="after"
+    # export YSU_MESSAGE_POSITION="after"
   '';
   home.file = {
     ".p10k.zsh" = {
