b55fac6cde4ac407b2645eac8422ee55a488898e
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-07-08 15:08:36 +0200

    fix wsl dns setup

--- flake.nix
+++ flake.nix
@@ -31,138 +31,152 @@
     # ladybird.url = "github:LadybirdBrowser/ladybird";
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, nixos-hardware, nixos-wsl, darwin
-    , home-manager, ... }@inputs:
+  outputs =
+    { nixpkgs
+    , nixpkgs-unstable
+    , nixos-hardware
+    , nixos-wsl
+    , darwin
+    , home-manager
+    , ...
+    }@inputs:
     let
       userName = "cn";
       description = "Christoffer Nissen";
       stateVersion = "24.11";
-    in {
-      darwinConfigurations = let
-        system = "aarch64-darwin"; # Change to "x86_64-darwin" if needed
-        unstable = import nixpkgs-unstable { inherit system; };
-      in {
-        mac = darwin.lib.darwinSystem {
-          system = system;
-          modules = [
+    in
+    {
+      darwinConfigurations =
+        let
+          system = "aarch64-darwin"; # Change to "x86_64-darwin" if needed
+          unstable = import nixpkgs-unstable { inherit system; };
+        in
+        {
+          mac = darwin.lib.darwinSystem {
+            system = system;
+            modules = [
 
-            { users.users.${userName} = { home = "/Users/${userName}"; }; }
+              { users.users.${userName} = { home = "/Users/${userName}"; }; }
 
-            ./hosts/mac/darwin-configuration.nix
+              ./hosts/mac/darwin-configuration.nix
 
-            home-manager.darwinModules.home-manager
-            {
-              home-manager.extraSpecialArgs = {
-                inherit inputs;
-                inherit unstable;
-                inherit system;
-                inherit userName;
-                inherit stateVersion;
-              };
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.${userName} = import ./home-manager/mac.nix;
-            }
-
-          ];
-          specialArgs = { inherit inputs; };
-        };
-      };
-      nixosConfigurations = let
-        system = "x86_64-linux";
-        unstable = import nixpkgs-unstable { inherit system; };
-      in {
-        x1 = nixpkgs.lib.nixosSystem {
-          system = system;
-          modules = [
-
-            # Create user
-            {
-              users.users.${userName} = {
-                isNormalUser = true;
-                description = description;
-                extraGroups = [ "networkmanager" "wheel" "docker" ];
-                home = "/home/${userName}";
-              };
-            }
-
-            # ref: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
-            nixos-hardware.nixosModules.lenovo-thinkpad-x1-9th-gen
-
-            # Create NixOS
-            {
-              nixpkgs = {
-                config = {
-                  allowUnfree = true;
-                  allowUnfreePredicate = (_: true);
+              home-manager.darwinModules.home-manager
+              {
+                home-manager.extraSpecialArgs = {
+                  inherit inputs;
+                  inherit unstable;
+                  inherit system;
+                  inherit userName;
+                  inherit stateVersion;
                 };
-              };
-            }
-            ./hosts/x1/configuration.nix
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.${userName} = import ./home-manager/mac.nix;
+              }
 
-            # Create home folder
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.extraSpecialArgs = {
-                inherit inputs;
-                inherit unstable;
-                inherit userName;
-                inherit stateVersion;
-              };
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.${userName} = import ./home-manager/linux.nix;
-            }
-
-          ];
+            ];
+            specialArgs = { inherit inputs; };
+          };
         };
-        wsl = nixpkgs.lib.nixosSystem {
-          system = system;
-          specialArgs = { inherit inputs nixos-wsl system userName; };
-          modules = [
+      nixosConfigurations =
+        let
+          system = "x86_64-linux";
+          unstable = import nixpkgs-unstable { inherit system; };
+        in
+        {
+          x1 = nixpkgs.lib.nixosSystem {
+            system = system;
+            modules = [
 
-            # Create User
-            {
-              users.users.${userName} = {
-                isNormalUser = true;
-                description = description;
-                extraGroups = [ "networkmanager" "wheel" "docker" ];
-                home = "/home/${userName}";
-              };
-            }
+              # Create user
+              {
+                users.users.${userName} = {
+                  isNormalUser = true;
+                  description = description;
+                  extraGroups = [ "networkmanager" "wheel" "docker" ];
+                  home = "/home/${userName}";
+                };
+              }
 
-            # basic configuration
-            ./hosts/wsl/configuration.nix
+              # ref: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
+              nixos-hardware.nixosModules.lenovo-thinkpad-x1-9th-gen
 
-            # wsl specific configuration
-            nixos-wsl.nixosModules.default
-            {
-              system.stateVersion = stateVersion;
-              wsl.enable = true;
-              wsl.defaultUser = userName;
+              # Create NixOS
+              {
+                nixpkgs = {
+                  config = {
+                    allowUnfree = true;
+                    allowUnfreePredicate = (_: true);
+                  };
+                };
+              }
+              ./hosts/x1/configuration.nix
 
-              # WSL Configuration
-              wsl.wslConf.automount.enabled = true;
-              wsl.wslConf.boot.systemd = true;
-              wsl.wslConf.network.generateResolvConf = true;
-            }
+              # Create home folder
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.extraSpecialArgs = {
+                  inherit inputs;
+                  inherit unstable;
+                  inherit userName;
+                  inherit stateVersion;
+                };
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.${userName} = import ./home-manager/linux.nix;
+              }
 
-            # Create home folder
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.extraSpecialArgs = {
-                inherit inputs;
-                inherit unstable;
-                inherit system;
-                inherit userName;
-                inherit stateVersion;
-              };
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.${userName} = import ./home-manager/wsl.nix;
-            }
-          ];
+            ];
+          };
+          wsl = nixpkgs.lib.nixosSystem {
+            system = system;
+            specialArgs = { inherit inputs nixos-wsl system userName; };
+            modules = [
+
+              # Create User
+              {
+                users.users.${userName} = {
+                  isNormalUser = true;
+                  description = description;
+                  extraGroups = [ "networkmanager" "wheel" "docker" ];
+                  home = "/home/${userName}";
+                };
+              }
+
+              # basic configuration
+              ./hosts/wsl/configuration.nix
+
+              # wsl specific configuration
+              nixos-wsl.nixosModules.default
+              {
+                system.stateVersion = stateVersion;
+                wsl.enable = true;
+                wsl.defaultUser = userName;
+
+                # WSL Configuration
+                wsl.wslConf.automount.enabled = true;
+                wsl.wslConf.boot.systemd = true;
+                wsl.wslConf.network.generateResolvConf = false;
+                networking.nameservers =
+                  [ "10.41.2.10" "10.41.2.11" "10.41.18.10" ];
+              }
+
+              # Create home folder
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.extraSpecialArgs = {
+                  inherit inputs;
+                  inherit unstable;
+                  inherit system;
+                  inherit userName;
+                  inherit stateVersion;
+                };
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.${userName} = import ./home-manager/wsl.nix;
+              }
+            ];
+          };
         };
-      };
     };
 }
