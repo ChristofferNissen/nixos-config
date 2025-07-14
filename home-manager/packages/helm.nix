1f45e26f48b4e3cb637c347bc8d9ea5b07aa24a3
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-07-14 09:29:49 +0200

    nix flake update

--- home-manager/packages/helm.nix
+++ home-manager/packages/helm.nix
@@ -2,9 +2,14 @@
 let
   my-kubernetes-helm = with pkgs;
     wrapHelm kubernetes-helm {
-      plugins = with pkgs.kubernetes-helmPlugins; [ helm-secrets helm-diff ];
+      plugins = with pkgs.kubernetes-helmPlugins; [
+        helm-secrets
+        helm-diff
+        helm-unittest
+      ];
     };
-in {
+in
+{
   home.packages = [
     my-kubernetes-helm
     # (pkgs.wrapHelm pkgs.kubernetes-helm { plugins = [
