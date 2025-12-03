193f6d45930ac0edb0687915d46e55046c878e46
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-12-03 09:29:22 +0100

    fix ghostty theme

--- home-manager/configs/term/ghostty.nix
+++ home-manager/configs/term/ghostty.nix
@@ -4,7 +4,7 @@
   home.packages = [ inputs.ghostty.packages."x86_64-linux".default ];
 
   home.file."./.config/ghostty/config".text = ''
-    theme = "dark:catppuccin-mocha,light:catppuccin-latte"
+    theme = "dark:Catppuccin Mocha,light:Catppuccin Latte"
     font-family = "Fira Code"
     font-size = 12
     shell-integration-features = no-cursor
